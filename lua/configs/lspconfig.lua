9a3a4ff18d0b88f8a743b4d11fe00a5c70d3c31e
trypophob1a <75798247+trypophob1a@users.noreply.github.com>
2024-12-06 18:34:54 +0500

    Update lspconfig.lua skip asm_lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,7 +8,7 @@ local fs = require "utility.fs"
 local checker = require "utility.modules_checker"
 
 local util = require "lspconfig/util"
-local skip = require("utility.skip_register").Add {}
+local skip = require("utility.skip_register").Add {"configs/user_lspconfigs/asm_config.lua"}
 fs.ReadFilesAsync(relativePath, "Error loading files", function(file)
   file = fs.RemoveLuaExtension(file)
   local success, cfg = pcall(require, "configs.user_lspconfigs." .. file)
